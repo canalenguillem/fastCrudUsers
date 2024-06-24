9decd9da103102c74333b631a6704b7f3714f6e4
enguillem <enguillem@gmail.com>
2024-06-24 20:32:55 +0200

    ya se pueden crear usuarios

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -1,21 +1,14 @@
 from sqlalchemy.orm import Session
-from app.models.user import User
-from app.schemas.user import UserCreate
-from fastapi import Depends, HTTPException, status
-from app.db.database import get_db
-from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
-from app.schemas.token import TokenData
+from app.models import user as models
+from app.schemas import user as schemas
+from fastapi import HTTPException, status
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
-from typing import Optional
+from app.models import profile as profile_models
+from decouple import config
 
-# Configuración de seguridad
-SECRET_KEY = "your-secret-key"  # Reemplaza esto con tu clave secreta real
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = config("SECRET_KEY")
+DEFAULT_PROFILE = config("DEFAULT_PROFILE")
 
 
 def verify_password(plain_password, hashed_password):
@@ -26,47 +19,53 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def authenticate_user(db: Session, username: str, password: str):
-    user = db.query(User).filter(User.email == username).first()
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = get_password_hash(user.password)
+    profile_id = user.profile_id
+    if not profile_id:
+        default_profile = db.query(profile_models.Profile).filter(
+            profile_models.Profile.name == DEFAULT_PROFILE).first()
+        if not default_profile:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Default profile not found")
+        profile_id = default_profile.id
+    db_user = models.User(email=user.email, hashed_password=hashed_password,
+                          username=user.username, profile_id=profile_id)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def update_user(db: Session, db_user: models.User, user_update: schemas.UserUpdate):
+    update_data = user_update.dict(exclude_unset=True)
+    if 'password' in update_data:
+        update_data['hashed_password'] = get_password_hash(
+            update_data.pop('password'))
+    for key, value in update_data.items():
+        setattr(db_user, key, value)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
         return False
     return user
-
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = db.query(User).filter(User.email == token_data.username).first()
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if not current_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
