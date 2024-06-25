90b902e4ad98ecc0049afe8792b5c1455a1d6a84
enguillem <enguillem@gmail.com>
2024-06-25 12:45:11 +0200

    getting tokens by blockchain id

--- backend/app/routers/erc20_token.py
+++ backend/app/routers/erc20_token.py
@@ -1,15 +1,23 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.schemas.erc20_token import TokenCreate, Token
-from app.crud.erc20_token import create_token
+from app.schemas.erc20_token import Token
+from app.crud.erc20_token import get_tokens_by_blockchain_id, create_token
 from app.db.database import get_db
 from app.routers.auth import get_current_admin_user
+from app.schemas.erc20_token import TokenCreate
 
 router = APIRouter()
 
-@router.post("/erc20_tokens/", response_model=Token, dependencies=[Depends(get_current_admin_user)])
+@router.post("/", response_model=Token, dependencies=[Depends(get_current_admin_user)])
 def create_erc20_token(token: TokenCreate, db: Session = Depends(get_db)):
     db_token = create_token(db, token)
     if not db_token:
         raise HTTPException(status_code=400, detail="Token could not be created")
     return db_token
+
+@router.get("/by_blockchain/{blockchain_id}", response_model=list[Token])
+def get_erc20_tokens_by_blockchain(blockchain_id: int, db: Session = Depends(get_db)):
+    tokens = get_tokens_by_blockchain_id(db, blockchain_id)
+    if not tokens:
+        raise HTTPException(status_code=404, detail="No tokens found for this blockchain")
+    return tokens
